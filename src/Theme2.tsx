bb9dec4103b429bf59a7cec82688397d8b10dc2c
dus532 <dus532@naver.com>
2024-09-20 17:44:46 +0900

    Feat: 골 액션

--- src/Theme2.tsx
+++ src/Theme2.tsx
@@ -27,142 +27,183 @@ function Theme2() {
       : `0${Math.floor(time / 60)}`;
   const sec = time % 60 > 9 ? time % 60 : `0${time % 60}`;
 
+  const [isGoal, setIsGoal] = useState(false);
+
+  function onGoal() {
+    setIsGoal(true);
+
+    setTimeout(() => {
+      setIsGoal(false);
+    }, 6000);
+  }
+
   return (
-    <div
-      style={{
-        marginTop: 32,
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
+    <div>
       <div
         style={{
+          marginTop: 32,
           display: 'flex',
-          height: 80,
+          alignItems: 'center',
+          flexDirection: 'column',
         }}
       >
         <div
           style={{
-            marginRight: '-40px',
             display: 'flex',
-            position: 'relative',
-            zIndex: 2,
-          }}
-        >
-          <div
-            style={{
-              width: 220,
-              height: '100%',
-              background: '#DD2121',
-              fontWeight: 700,
-              fontFamily: 'Paperlogy',
-              fontSize: 48,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            맨유
-          </div>
-          <Triangle />
-        </div>
-        <div
-          style={{
+            height: 80,
+            width: 'auto',
             background: 'black',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: 260,
+            position: 'relative',
           }}
         >
           <div
             style={{
               display: 'flex',
               alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: 136,
-              fontWeight: 900,
-              position: 'relative',
-              gap: 12,
-              zIndex: 3,
-            }}
-          >
-            <div
-              style={{
-                textAlign: 'right',
-                position: 'relative',
-                top: -8,
-                width: 100,
-              }}
-            >
-              {ranNum}
-            </div>
-            <LogoEPL />
-            <div
-              style={{
-                textAlign: 'left',
-                position: 'relative',
-                top: -8,
-                width: 100,
-              }}
-            >
-              0
-            </div>
-          </div>
-        </div>
-        <div style={{ display: 'flex', position: 'relative' }}>
-          <div
-            style={{
+              justifyContent: 'flex-end',
+              width: '100%',
               height: '100%',
-              left: '0px',
               position: 'absolute',
+              right: 60,
+              top: 0,
+              fontSize: 60,
+              fontWeight: 900,
+              fontFamily: 'Pretendard',
             }}
+            className={isGoal ? 'typoGoal' : ''}
           >
-            <Triangle fill='#3867A9' />
+            GOAL
           </div>
           <div
             style={{
-              width: 260,
-              height: '100%',
-              background: '#87B3E2',
-              fontWeight: 700,
-              fontFamily: 'Paperlogy',
-              fontSize: 48,
+              marginRight: '-40px',
+              display: 'flex',
+              position: 'relative',
+              zIndex: 2,
+            }}
+          >
+            <div
+              style={{
+                width: 220,
+                height: '100%',
+                background: '#DD2121',
+                fontWeight: 700,
+                fontFamily: 'Paperlogy',
+                fontSize: 48,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              맨유
+            </div>
+            <Triangle />
+          </div>
+          <div
+            style={{
+              background: 'black',
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              color: '#0B223A',
+              width: 260,
             }}
           >
-            맨시티
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: 136,
+                fontWeight: 900,
+                position: 'relative',
+                gap: 12,
+                zIndex: 3,
+              }}
+              className={isGoal ? 'opacity' : ''}
+            >
+              <div
+                style={{
+                  textAlign: 'right',
+                  position: 'relative',
+                  top: -8,
+                  width: 100,
+                }}
+              >
+                {ranNum}
+              </div>
+              <LogoEPL />
+              <div
+                style={{
+                  textAlign: 'left',
+                  position: 'relative',
+                  top: -8,
+                  width: 100,
+                }}
+              >
+                0
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              position: 'relative',
+            }}
+            className={isGoal ? 'opacity' : ''}
+          >
+            <div
+              style={{
+                height: '100%',
+                left: '0px',
+                position: 'absolute',
+              }}
+            >
+              <Triangle fill='#3867A9' />
+            </div>
+            <div
+              style={{
+                width: 260,
+                height: '100%',
+                background: '#87B3E2',
+                fontWeight: 700,
+                fontFamily: 'Paperlogy',
+                fontSize: 48,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: '#0B223A',
+              }}
+            >
+              맨시티
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          width: 220,
-          marginRight: '20px',
-        }}
-      >
         <div
           style={{
-            width: '100%',
-            height: 64,
-            background: '#421041',
             display: 'flex',
-            alignItems: 'center',
             justifyContent: 'center',
-            fontSize: 40,
-            fontWeight: 700,
-            fontFamily: 'Chivo Mono',
+            width: 220,
+            marginRight: '20px',
           }}
         >
-          {min}:{sec}
+          <div
+            style={{
+              width: '100%',
+              height: 64,
+              background: '#421041',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontSize: 40,
+              fontWeight: 700,
+              fontFamily: 'Chivo Mono',
+            }}
+          >
+            {min}:{sec}
+          </div>
         </div>
       </div>
+      <button onClick={onGoal}>골</button>
     </div>
   );
 }
